7956a3767d1b7627fdfce2ff124b991a14d7857c
Fawad Ghori <fawadghori@folio3.com>
2025-11-28 18:59:08 +0500

    modern look changes

--- BlazorWPFApp/MainWindow.xaml.cs
+++ BlazorWPFApp/MainWindow.xaml.cs
@@ -58,13 +58,16 @@ namespace BlazorWPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddWpfBlazorWebView();
 
             #if DEBUG
             serviceCollection.AddBlazorWebViewDeveloperTools();
-            #endif
+#endif
 
             // Set services to the BlazorWebView
             blazorWebView.Services = serviceCollection.BuildServiceProvider();
