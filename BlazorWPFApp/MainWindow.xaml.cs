8750899788d7cd5ef161c9b0788b6608b28b9c80
Fawad Ghori <fawadghori@folio3.com>
2025-11-28 19:14:15 +0500

    unused code removed

--- BlazorWPFApp/MainWindow.xaml.cs
+++ BlazorWPFApp/MainWindow.xaml.cs
@@ -1,53 +1,4 @@
-﻿//using System.Text;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
-
-//namespace BlazorWPFApp
-//{
-//    /// <summary>
-//    /// Interaction logic for MainWindow.xaml
-//    /// </summary>
-//    public partial class MainWindow : Window
-//    {
-//        public MainWindow()
-//        {
-//            InitializeComponent();
-//        }
-//    }
-//}
-
-
-//using Microsoft.Extensions.DependencyInjection;
-//using System.Windows;
-//using Microsoft.AspNetCore.Components.WebView.Wpf;
-
-//namespace BlazorWPFApp
-//{
-//    public partial class MainWindow : Window
-//    {
-//        public MainWindow()
-//        {
-//            //InitializeComponent();
-
-//            var serviceCollection = new ServiceCollection();
-//            serviceCollection.AddWpfBlazorWebView();
-//#if DEBUG
-//            serviceCollection.AddBlazorWebViewDeveloperTools();
-//#endif
-
-//            Resources.Add("services", serviceCollection.BuildServiceProvider());
-//        }
-//    }
-//}
-
-using Microsoft.AspNetCore.Components.WebView.Wpf;
+﻿using Microsoft.AspNetCore.Components.WebView.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
@@ -67,7 +18,7 @@ namespace BlazorWPFApp
 
             #if DEBUG
             serviceCollection.AddBlazorWebViewDeveloperTools();
-#endif
+            #endif
 
             // Set services to the BlazorWebView
             blazorWebView.Services = serviceCollection.BuildServiceProvider();
