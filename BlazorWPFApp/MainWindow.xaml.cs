f1835cfbc7d500eaa8c3afb394278e9d3505e67a
Fawad Ghori <fawadghori@folio3.com>
2025-11-28 18:45:26 +0500

    Add project files.

---
+++ BlazorWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿//using System.Text;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+//namespace BlazorWPFApp
+//{
+//    /// <summary>
+//    /// Interaction logic for MainWindow.xaml
+//    /// </summary>
+//    public partial class MainWindow : Window
+//    {
+//        public MainWindow()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
+
+
+//using Microsoft.Extensions.DependencyInjection;
+//using System.Windows;
+//using Microsoft.AspNetCore.Components.WebView.Wpf;
+
+//namespace BlazorWPFApp
+//{
+//    public partial class MainWindow : Window
+//    {
+//        public MainWindow()
+//        {
+//            //InitializeComponent();
+
+//            var serviceCollection = new ServiceCollection();
+//            serviceCollection.AddWpfBlazorWebView();
+//#if DEBUG
+//            serviceCollection.AddBlazorWebViewDeveloperTools();
+//#endif
+
+//            Resources.Add("services", serviceCollection.BuildServiceProvider());
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace BlazorWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+
+            #if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+            #endif
+
+            // Set services to the BlazorWebView
+            blazorWebView.Services = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
